163ca0b6cc9229ac471d8e75e83ebc6a440d6ed7
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-11-13 17:02:20 +0100

    4.5

--- src/main/java/dev/lightdream/api/dto/TestBattery.java
+++ src/main/java/dev/lightdream/api/dto/TestBattery.java
@@ -2,6 +2,7 @@ package dev.lightdream.api.dto;
 
 import dev.lightdream.api.IAPI;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.fusesource.jansi.Ansi;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -20,7 +21,9 @@ public class TestBattery {
         AtomicInteger testCount = new AtomicInteger();
         tests.forEach(test -> {
             test.test();
-            api.getLogger().info("Test " + testCount + ": " + (test.status ? "Passed" : "Failed"));
+            api.getLogger().info("Test " + testCount + ": " + (test.status ?
+                    Ansi.ansi().fg(Ansi.Color.GREEN).boldOff() + "Passed" :
+                    Ansi.ansi().fg(Ansi.Color.RED).boldOff() + "Failed"));
             testCount.getAndIncrement();
         });
     }
