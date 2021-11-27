6d5aa6a87b3d87e4f69caf4928a76d2e95357f8b
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-11-27 18:29:32 +0100

    4.46

--- src/main/java/dev/lightdream/api/dto/TestBattery.java
+++ src/main/java/dev/lightdream/api/dto/TestBattery.java
@@ -1,7 +1,7 @@
 package dev.lightdream.api.dto;
 
 import dev.lightdream.api.IAPI;
-import dev.lightdream.api.utils.Logger;
+import dev.lightdream.api.utils.Debugger;
 import org.fusesource.jansi.Ansi;
 
 import java.util.List;
@@ -19,13 +19,16 @@ public class TestBattery {
 
     public void test() {
         AtomicInteger testCount = new AtomicInteger();
+        StringBuilder output = new StringBuilder();
         tests.forEach(test -> {
             test.test();
-            Logger.info("\nTest " + testCount + ": " + (test.status ?
+            //noinspection StringConcatenationInsideStringBufferAppend
+            output.append("\nTest " + testCount + ": " + (test.status ?
                     Ansi.ansi().fg(Ansi.Color.GREEN).boldOff() + "Passed" + Ansi.ansi().fg(Ansi.Color.DEFAULT).boldOff() :
                     Ansi.ansi().fg(Ansi.Color.RED).boldOff() + "Failed\nResult: " + test.result + "\nExpected result: " + test.expectedResult) + Ansi.ansi().fg(Ansi.Color.DEFAULT).boldOff());
             testCount.getAndIncrement();
         });
+        Debugger.info(output.toString());
     }
 
 
