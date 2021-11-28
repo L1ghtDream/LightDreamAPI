0f187914e0dea5cbf770fb865af821650af3e90f
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-11-28 09:17:19 +0100

    4.49

--- src/main/java/dev/lightdream/api/managers/database/HikariDatabaseManager.java
+++ src/main/java/dev/lightdream/api/managers/database/HikariDatabaseManager.java
@@ -127,8 +127,6 @@ public abstract class HikariDatabaseManager extends DatabaseManager {
                     continue;
                 }
                 DatabaseField databaseField = field.getAnnotation(DatabaseField.class);
-                Debugger.info(databaseField.columnName());
-                Debugger.info(field.getName());
                 field.set(obj, getObject(field.getType(), rs.getObject(databaseField.columnName())));
             }
             DatabaseEntry entry = (DatabaseEntry) obj;
