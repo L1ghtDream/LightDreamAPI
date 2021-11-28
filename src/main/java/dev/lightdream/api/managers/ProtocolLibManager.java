202fcc16ae627b5cd81adc5aaa538a267fc8cb6e
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-11-28 17:20:44 +0100

    4.59

--- src/main/java/dev/lightdream/api/managers/ProtocolLibManager.java
+++ src/main/java/dev/lightdream/api/managers/ProtocolLibManager.java
@@ -17,45 +17,25 @@ public class ProtocolLibManager {
         protocolManager = ProtocolLibrary.getProtocolManager();
     }
 
-    @SuppressWarnings({"unused", "deprecation"})
+    @SuppressWarnings({"unused"})
     @SneakyThrows
-    public void sendWorldBorder(Player player, double x, double z, double newDiam) {
-        PacketContainer packet1 = protocolManager.createPacket(PacketType.Play.Server.WORLD_BORDER);
-        PacketContainer packet2 = protocolManager.createPacket(PacketType.Play.Server.WORLD_BORDER);
+    public void sendWorldBorder(Player player, double x, double z, double diameter) {
 
-        packet1.getWorldBorderActions().writeSafely(0, EnumWrappers.WorldBorderAction.SET_CENTER);
-        packet2.getWorldBorderActions().writeSafely(0, EnumWrappers.WorldBorderAction.SET_SIZE);
+        PacketContainer packet1 = protocolManager.createPacket(PacketType.Play.Server.SET_BORDER_CENTER);
+        PacketContainer packet2 = protocolManager.createPacket(PacketType.Play.Server.SET_BORDER_SIZE);
 
         packet1.getDoubles().write(0, x);
         packet1.getDoubles().write(1, z);
 
-        packet2.getDoubles().write(0, 0.0);
-        packet2.getDoubles().write(1, newDiam);
-
-        packet2.getLongs().write(0, 0L);
+        packet2.getDoubles().write(0, diameter);
 
         protocolManager.sendServerPacket(player, packet1);
         protocolManager.sendServerPacket(player, packet2);
     }
 
-    @SuppressWarnings({"unused", "deprecation"})
+    @SuppressWarnings({"unused"})
     @SneakyThrows
     public void sendWorldBorder(Player player, PluginLocation location, double newDiam) {
-        PacketContainer packet1 = protocolManager.createPacket(PacketType.Play.Server.WORLD_BORDER);
-        PacketContainer packet2 = protocolManager.createPacket(PacketType.Play.Server.WORLD_BORDER);
-
-        packet1.getWorldBorderActions().writeSafely(0, EnumWrappers.WorldBorderAction.SET_CENTER);
-        packet2.getWorldBorderActions().writeSafely(0, EnumWrappers.WorldBorderAction.SET_SIZE);
-
-        packet1.getDoubles().write(0, location.x);
-        packet1.getDoubles().write(1, location.z);
-
-        packet2.getDoubles().write(0, 0.0);
-        packet2.getDoubles().write(1, newDiam);
-
-        packet2.getLongs().write(0, 0L);
-
-        protocolManager.sendServerPacket(player, packet1);
-        protocolManager.sendServerPacket(player, packet2);
+        sendWorldBorder(player,location.x, location.z, newDiam);
     }
 }
