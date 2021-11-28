2b266c8b0f5c1f0d3cbc4066a196e44ec3e56f64
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-11-28 17:50:10 +0100

    4.64

--- src/main/java/dev/lightdream/api/managers/ProtocolLibManager.java
+++ src/main/java/dev/lightdream/api/managers/ProtocolLibManager.java
@@ -24,6 +24,7 @@ public class ProtocolLibManager {
         PacketContainer packet1 = protocolManager.createPacket(PacketType.Play.Server.WORLD_BORDER);
         PacketContainer packet2 = protocolManager.createPacket(PacketType.Play.Server.WORLD_BORDER);
         PacketContainer packet3 = protocolManager.createPacket(PacketType.Play.Server.WORLD_BORDER);
+
         packet1.getWorldBorderActions().writeSafely(0, EnumWrappers.WorldBorderAction.INITIALIZE);
         packet2.getWorldBorderActions().writeSafely(0, EnumWrappers.WorldBorderAction.SET_CENTER);
         packet3.getWorldBorderActions().writeSafely(0, EnumWrappers.WorldBorderAction.SET_SIZE);
@@ -34,10 +35,11 @@ public class ProtocolLibManager {
                 .write(2, diameter)
                 .write(3, diameter);
         packet1.getLongs()
-                .write(0, 1000000000000000000L);
+                .write(0, 0L);
         packet1.getIntegers()
-                .write(0, 0)
-                .write(1, 0);
+                .write(0,29999984)
+                .write(1, 0)
+                .write(2, 0);
 
         packet2.getDoubles()
                 .write(0, x)
@@ -48,8 +50,8 @@ public class ProtocolLibManager {
                 .write(0, diameter);
 
         protocolManager.sendServerPacket(player, packet1);
-        protocolManager.sendServerPacket(player, packet2);
-        protocolManager.sendServerPacket(player, packet3);
+        //protocolManager.sendServerPacket(player, packet2);
+        //protocolManager.sendServerPacket(player, packet3);
     }
 
     @SuppressWarnings({"unused"})
